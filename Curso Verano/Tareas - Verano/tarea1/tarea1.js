7cb2711be4ef0da7d7e9ea7469743f01c1ccefb6
anasofia01 <sofiapacheco0423@gmail.com>
2025-07-13 17:27:53 -0500

    feat: se crea la estructura del forms en html y se recupera la info del forms en js

---
+++ Curso Verano/Tareas - Verano/tarea1/tarea1.js
@@ -0,0 +1,5 @@
+const formulario = document.getElementById('formulario-info');
+const inputNombre = document.getElementById('nombre-forms');
+const inputEmail = document.getElementById('email-forms');
+const FraseFav = document.getElementById('frase-fav');
+const selectFecha = document.getElementById('select-fecha');
