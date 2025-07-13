ca53c1aefba8531552e6a3f8fbf39bc683fe7176
anasofia01 <sofiapacheco0423@gmail.com>
2025-07-13 17:32:37 -0500

    feat: se captura el evento del formulario

--- Curso Verano/Tareas - Verano/tarea1/tarea1.js
+++ Curso Verano/Tareas - Verano/tarea1/tarea1.js
@@ -1,5 +1,23 @@
 const formulario = document.getElementById('formulario-info');
 const inputNombre = document.getElementById('nombre-forms');
 const inputEmail = document.getElementById('email-forms');
-const FraseFav = document.getElementById('frase-fav');
+const fraseFav = document.getElementById('frase-fav');
 const selectFecha = document.getElementById('select-fecha');
+
+formulario.addEventListener('submit', function (event) {
+	event.preventDefault();
+	const infoNombre = inputNombre.value;
+	const infoEmail = inputEmail.value;
+	const infoFrase = fraseFav.value;
+	const infoSelect = selectFecha.value;
+	console.log(
+		'su nombre es' +
+			infoNombre +
+			', su email es ' +
+			infoEmail +
+			', su frase fav es ' +
+			infoFrase +
+			'y su fecha de nacimiento es ' +
+			infoSelect
+	);
+});
