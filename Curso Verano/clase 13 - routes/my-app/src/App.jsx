6c6015bc48d5bc3db67c8b2b4beb831e37da7f20
anasofia01 <sofiapacheco0423@gmail.com>
2025-07-30 19:31:54 -0500

    feat: se crea clase 13 sobre las rutas

---
+++ Curso Verano/clase 13 - routes/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Blog from './pages/blog';
+
+function App() {
+	return (
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/blog' element={<Blog />} />
+		</Routes>
+	);
+}
+
+export default App;
